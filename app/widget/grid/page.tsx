4377796fe4c4177ded0a7b6636dc3d50c214ab58
AMattiza <andreas@choose-hub.com>
2025-08-24 18:52:34 +0200

    Update page.tsx

--- app/widget/grid/page.tsx
+++ app/widget/grid/page.tsx
@@ -6,11 +6,22 @@ import GridInner from "./grid-inner";
 function GridPageContent() {
   return (
     <>
-      {/* Child-Script für iFrameResizer (muss zur Parent-Version passen) */}
+      {/* Initialisierung: Lizenz + auto height */}
+      <Script id="iframe-resizer-init" strategy="beforeInteractive">
+        {`
+          window.iframeResizer = {
+            license: 'GPLv3',
+            heightCalculationMethod: 'auto'
+          };
+        `}
+      </Script>
+
+      {/* Child-Skript (muss exakt die gleiche Version wie im Parent haben) */}
       <Script
         src="https://cdn.jsdelivr.net/npm/iframe-resizer@4.3.9/js/iframeResizer.contentWindow.min.js"
         strategy="beforeInteractive"
       />
+
       <GridInner />
     </>
   );
