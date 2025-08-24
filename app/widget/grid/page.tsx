ef413e3e0de5d4733ba788f352dcec28e0ad7639
AMattiza <andreas@choose-hub.com>
2025-08-24 16:51:44 +0200

    Update page.tsx

--- app/widget/grid/page.tsx
+++ app/widget/grid/page.tsx
@@ -1,19 +1,16 @@
 "use client";
 import Script from "next/script";
-import { Suspense } from "react";
 import GridInner from "./grid-inner";
 
 export default function GridPage() {
   return (
     <>
-      {/* iFrame-Resizer Child-Script */}
+      {/* Pflicht: Child-Script für automatische Höhe */}
       <Script
         src="https://cdn.jsdelivr.net/npm/iframe-resizer/js/iframeResizer.contentWindow.min.js"
         strategy="beforeInteractive"
       />
-      <Suspense fallback={<div style={{ padding: 16, textAlign: "center" }}>Lade…</div>}>
-        <GridInner />
-      </Suspense>
+      <GridInner />
     </>
   );
 }
