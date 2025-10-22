5b2a88ca4d0c7f8f42de115ec304e09fa22b1713
AMattiza <andreas@choose-hub.com>
2025-10-22 19:43:27 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  swcMinify: false,
-
-  experimental: {
-    esmExternals: false
-  },
-
-  compiler: {
-    removeConsole: false,
-    styledComponents: true
-  },
-
+  swcMinify: false, // verhindert fehlerhafte Minifizierung
+  compress: true,
+  experimental: { esmExternals: false },
   headers: async () => [
     {
       source: "/(.*)",
       headers: [
-        { key: "X-Frame-Options", value: "ALLOWALL" }
+        { key: "X-Frame-Options", value: "ALLOWALL" },
+        { key: "Content-Encoding", value: "" } // verhindert doppelte Brotli-Angabe
       ]
     }
   ]
 };
-
 export default nextConfig;
